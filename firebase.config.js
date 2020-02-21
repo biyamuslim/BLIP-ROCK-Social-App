58fcbc33004de272d5222dbb06b05b480d020887
Bareerah Muslim <biyamuslim@gmail.com>
2020-02-21 18:25:30 +0500

    Add files via upload

---
+++ firebase.config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC0uFqelWOCU1RjoUpVwFgu7UfbQzOtZaU",
+    authDomain: "blip-rock.firebaseapp.com",
+    databaseURL: "https://blip-rock.firebaseio.com",
+    projectId: "blip-rock",
+    storageBucket: "blip-rock.appspot.com",
+    messagingSenderId: "247765753433",
+    appId: "1:247765753433:web:c8cac905f804e27c645b48"
+  };
